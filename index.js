cba648cbc7175856f04ba40ce0a1039da4230680
joejoy <joejoy456999@gmail.com>
2021-04-27 18:43:14 +0530

    done

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const app = express()
 app.set("views",path.join(__dirname,"views"))
 app.set("view engine","ejs")
 
-// If you do not want to use diskStorage then uncomment it
-    
+// If you do not want to use diskStorage then uncomment it    
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {  
         // Uploads is the Upload_folder_name
@@ -31,30 +30,26 @@ var upload = multer({
         var filetypes = /jpeg|jpg|png/;
         var mimetype = filetypes.test(file.mimetype);  
         var extname = filetypes.test(path.extname(
-                    file.originalname).toLowerCase());
-        
+                    file.originalname).toLowerCase());        
         if (mimetype && extname) {
             return cb(null, true);
-        }
-      
+        }      
         cb("Error: File upload only supports the "
                 + "following filetypes - " + filetypes);
       }   
 // mypic is the name of file attribute
 }).single("mypic");   
 
+
 app.get("/",function(req,res){
     res.render("index.ejs");
 })    
 
-app.post("/uploadProfilePicture",function (req, res, next) {
-        
+app.post("/uploadProfilePicture",function (req, res, next) {        
     // Error MiddleWare for multer file upload, so if any
     // error occurs, the image would not be uploaded!
-    upload(req,res,function(err) {
-  
-        if(err) {
-  
+    upload(req,res,(err) => {  
+        if(err) {  
             // ERROR occured (here it can be occured due
             // to uploading image of size greater than
             // 1MB or uploading different file type)
